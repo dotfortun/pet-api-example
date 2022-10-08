b2f97e1e25ec2ec9997dad4fff47026578c8b0bf
dotfortun <shane.bell.dev@gmail.com>
2022-10-08 18:04:51 +0000

    Added pagination.

--- src/front/js/pages/pets.js
+++ src/front/js/pages/pets.js
@@ -3,10 +3,17 @@ import { PetCard } from "../component/pet";
 
 const PetsPage = () => {
   const [pets, setPets] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const decPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
 
   useEffect(() => {
     setInterval(() => {
-      fetch(process.env.BACKEND_URL + "/api/pets")
+      fetch(process.env.BACKEND_URL + `/api/pets?page=${page}`)
         .then((resp) => resp.json())
         .then((data) => setPets(data.pets));
     }, 1000);
@@ -14,6 +21,22 @@ const PetsPage = () => {
 
   return (
     <div className="container">
+      <div className="row my-3">
+        <div className="col col-10 offset-1 d-flex flex-row justify-content-around flex-wrap">
+          <div className="btn-group" role="group" aria-label="navigation">
+            <button
+              onClick={() => setPage(page + 1)}
+              type="button"
+              className="btn btn-primary"
+            >
+              Next Page
+            </button>
+            <button onClick={decPage} type="button" className="btn btn-primary">
+              Previous Page
+            </button>
+          </div>
+        </div>
+      </div>
       <div className="row">
         <div className="col col-10 offset-1 d-flex flex-row justify-content-between flex-wrap">
           {pets.map((elem, idx) => (
